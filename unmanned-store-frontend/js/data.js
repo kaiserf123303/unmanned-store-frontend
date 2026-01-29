94cfbf5601b4571052d9a669f02a1b0e0d5df5c6
kaiserf123303 <f123303g@gmail.com>
2026-01-29 13:24:39 +0800

    Add files via upload

---
+++ unmanned-store-frontend/js/data.js
@@ -0,0 +1,23 @@
+const stores = [
+  {
+    id: "S001",
+    name: "台北車站無人商店",
+    location: "台北車站",
+    status: "normal",
+    products: 12
+  },
+  {
+    id: "S002",
+    name: "內湖科技園區無人商店",
+    location: "內湖",
+    status: "low",
+    products: 8
+  },
+  {
+    id: "S003",
+    name: "信義商辦無人商店",
+    location: "信義區",
+    status: "error",
+    products: 5
+  }
+];
